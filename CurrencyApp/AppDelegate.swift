f793f8aa52753c024945fc75ff08da8cf49e4d23
Dmitry Kuzin <kuzindo@gmail.com>
2020-08-24 14:40:44 +0300

    Project config

---
+++ CurrencyApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CurrencyApp
+//
+//  Created by Dmitry Kuzin on 24.08.2020.
+//  Copyright © 2020 Dmitry Kuzin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
---
+++ CurrencyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyApp
+//
+//  Created by Dmitry Kuzin on 24.08.2020.
+//  Copyright © 2020 Dmitry Kuzin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
